46f3cf59221cd6b8b8a36af361baea9771bbbce4
remy <remy.dernat@univ-montp2.fr>
2017-03-01 15:45:14 +0100

    adding extration file

--- linpack/array200_200/get_flops.sh
+++ linpack/array200_200/get_flops.sh
@@ -5,5 +5,5 @@ max=`awk '$1 ~ "[0-9]+" {print $6}' $1 |sort -k6 |tail -1`
 
 echo "min:"$min
 echo "max:"$max
-echo -n "moyenne:"
+echo -n "average:"
 awk '$1 ~ "[0-9]+" {total=total+$6;n=n+1;} END{print total/n;}' $1
