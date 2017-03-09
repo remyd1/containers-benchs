f89c09dc9257b9b3186c91e565a00ed389190e31
remy <remy.dernat@univ-montp2.fr>
2017-03-09 14:10:29 +0100

    moving some stuff

--- linpack/linpacknewc/array200_200/get_flops.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-min=`awk '$1 ~ "[0-9]+" {print $6}' $1 |sort -k6 |head -1`
-max=`awk '$1 ~ "[0-9]+" {print $6}' $1 |sort -k6 |tail -1`
-
-echo "min:"$min
-echo "max:"$max
-echo -n "average:"
-awk '$1 ~ "[0-9]+" {total=total+$6;n=n+1;} END{print total/n;}' $1
