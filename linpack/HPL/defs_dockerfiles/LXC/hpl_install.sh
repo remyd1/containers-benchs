92db43eb3964cae6aa1b278ef43ad3d110587f08
remy <remy.dernat@univ-montp2.fr>
2017-03-10 15:13:25 +0100

    update hpl_install code

--- linpack/HPL/defs_dockerfiles/LXC/hpl_install.sh
+++ linpack/HPL/defs_dockerfiles/LXC/hpl_install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 ## Install the basics
 
-apt-get install -y  bash wget openssh-server gcc gfortran binutils build-essential \ 
-	time libc6-dev libgcc-5-dev libopenmpi-dev openmpi-common \ 
-	openmpi-bin openmpi-doc libatlas3-base libatlas-base-dev \ 
+apt-get install -y  bash wget openssh-server gcc gfortran binutils build-essential \
+	time libc6-dev libgcc-5-dev libopenmpi-dev openmpi-common \
+	openmpi-bin openmpi-doc libatlas3-base libatlas-base-dev \
 	libatlas-dev libatlas-doc
 
 ##Install OpenMPI
@@ -15,9 +15,10 @@ cd openmpi-2.0.2/
 mkdir build
 cd build
 ../configure --prefix=/usr/local
+make clean
 make all install
 
-adduser --disabled-password --gecos "" mpirun && \ 
+adduser --disabled-password --gecos "" mpirun && \
 	echo "mpirun ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
 
 #Set OpenMPI ENV
@@ -28,11 +29,10 @@ wget http://www.netlib.org/benchmark/hpl/hpl-2.2.tar.gz
 tar -xvf hpl-2.2.tar.gz -C /usr/local/
 mkdir -p /home/benchmark
 
+cd /usr/local/hpl-2.2/
 wget 'https://raw.githubusercontent.com/ArangoGutierrez/containers-benchs/master/linpack/HPL/defs_dockerfiles/singularity/Make.Linux' -O Make.Linux
 
-cd /usr/local/hpl-2.2/
-sed -ri "s|$(LN_S) $(TOPdir)/Make.$(arch) Make.inc|$(LN_S) /usr/local/hpl-2.2/Make.$(arch) Make.inc|" Make.top
+sed -i 's|$(LN_S) $(TOPdir)/Make.$(arch) Make.inc|$(LN_S) /usr/local/hpl-2.2/Make.$(arch) Make.inc|' Make.top
 
-make arch=Linux clean_arch_all 
+make arch=Linux clean_arch_all
 make arch=Linux
-
