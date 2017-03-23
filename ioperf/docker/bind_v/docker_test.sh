3ff3242f22c990de3d648e79fe2fbdf75dca0666
ArangoGutierrez <carlos.arango.gutierrez@correounivalle.edu.co>
2017-03-23 11:06:53 -0500

    ioperf and memory

---
+++ ioperf/docker/bind_v/docker_test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Iozone: Performance Test of File I/O" > docker_io.out
+echo "Version $Revision: 3.465 $" >> docker_io.out
+echo "Compiled for 64 bit mode." >> docker_io.out
+echo "Build: linux-AMD64" >> docker_io.out
+echo "	" >> docker_io.out
+echo "                                                              random    random     bkwd    record    stride                                    
+              kB  reclen    write  rewrite    read    reread    read     write     read   rewrite      read   fwrite frewrite    fread  freread" >> docker_io.out
+for i in {1..10}
+do
+	docker run --rm -v /tmp:/tmp ashael/iozone ./iozone –Ra –g 2G -U /tmp -f /tmp/testfile > tmp.out && sed -n '26,27 p' tmp.out >> docker_io.out
+done
---
+++ ioperf/singularity/no_bind_v/singularity_io_test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Iozone: Performance Test of File I/O" > singularity_io.out
+echo "Version $Revision: 3.465 $" >> singularity_io.out
+echo "Compiled for 64 bit mode." >> singularity_io.out
+echo "Build: linux-AMD64" >> singularity_io.out
+echo "	" >> singularity_io.out
+echo "                                                              random    random     bkwd    record    stride                                    
+              kB  reclen    write  rewrite    read    reread    read     write     read   rewrite      read   fwrite frewrite    fread  freread" >> singularity_io.out
+for i in {1..10}
+do
+	singularity exec -c ../iozone.img iozone –Ra –g 2G > tmp.out && sed -n '26,27 p' tmp.out >> singularity_io.out
+done
+
